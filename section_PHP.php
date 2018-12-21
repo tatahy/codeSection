3db79f3c80a69840f4fd99da9dbe0295dddd4e99
tatahy <hy135@139.com>
2018-12-21 23:45:52 +0800

    update

--- section_PHP.php
+++ section_PHP.php
@@ -157,3 +157,95 @@ PHP中的许多预定义变量都是"超全局的"，这意味着它们在一个
 
 
 <!--//  HY 2018/7/26 -->
+<?php
+json_encode($data, JSON_UNESCAPED_UNICODE) ;//第2个参数表示保持$data的现有显示，不进行unicode转码显示。
+json_last_error(); //显示最后的json错误码
+
+
+
+<!--//  HY 2018/12/11 -->
+#闭包+递归，实现从数组的根节点（默认为空,可指定任意名称）开始，得到该数组从根节点开始到最后一层节点的所有路径的数组。
+//对数组有结构要求:要求最后一层节点下保存的是关联数组
+//使用2个地址引用变量：&$pathArr存储每次递归结果，&$pathFind实现闭包的递归调用
+
+
+$test=["ass"=>["read"=>1],"att"=>["upload"=>1],"iss"=>["pat"=>["edit"=>1],"pro"=>["edit"=>1],"the"=>["edit"=>1]],"pro"=>["edit"=>1,"audit"=>1],"the"=>["edit"=>1,"audit"=>1,"execute"=>1]];
+
+$pathArr=[];
+
+#实现闭包的递归调用
+$pathFind=function($arr,$nodeName='')use(&$pathArr,&$pathFind){
+    $subKeys=[];
+    $subVals=[];
+        
+    if(is_array($arr)){
+        $subKeys=array_keys($arr);
+        $subVals=array_values($arr);
+    }else{
+        //递归调用本函数终止
+        return false;
+    }
+	#判断当前节点名是否为最后一层的节点名。不是最后一层的节点名，返回本层节点名与下一层节点名组合后的数组，递归操作
+    if(is_string($subKeys[0]) && is_array($subVals[0])){
+        //先删除当前节点名。
+        array_splice($pathArr,array_search($nodeName,$pathArr),1);
+        foreach($subKeys as $v){
+            if($v!='admin' && !empty($nodeName)){
+              $adStr=$nodeName.'-'.$v;
+            }else if($v!='admin' && empty($nodeName)){
+              $adStr=$v;
+            }
+            array_push($pathArr,$adStr);
+            //递归调用本闭包
+			$pathFind($arr[$v],$adStr);
+        }
+    }
+};
+#调用闭包函数，设定$pathArr的值。
+$pathFind($test);
+#得到结果：$pathArr=['ass','att','iss-pat','iss-pro','iss-the','pro','the']
+
+$dbAuth=["ass"=>["read"=>1],"att"=>["upload"=>1],"iss"=>["pat"=>["edit"=>1],"pro"=>["edit"=>1],"the"=>["edit"=>1]],"pro"=>["edit"=>1,"audit"=>1],"the"=>["edit"=>1,"audit"=>1,"execute"=>1]];
+
+$conAuth=["_ASS"=>["read"=>0,"write"=>0],"_ATT"=>["upload"=>0],"_ISS"=>["_PAT"=>["edit"=>1],"_PRO"=>["edit"=>1],"_THE"=>["edit"=>1]],"_PRO"=>["edit"=>1,"audit"=>1],"_THE"=>["edit"=>1,"audit"=>1,"execute"=>1],"_ADMIN"=>["enable"=>0]];
+
+/**
+     * 比较$dbAuth数组和$conAuth，补足$dbAuth中缺失的部分。
+	 * $dbAuth数组和$conAuth
+     * @param  Array $dbAuth，目标多维关联数组（json对象转换来的数组），地址引用。
+     * @param  Array $conAuth，基准多维关联数组，地址引用。
+     * @return Array $dbAuth, 补足空缺后的$dbAuth数组。
+     * 递归，
+     */
+function fn_merge_auth(&$dbAuth,&$conAuth)
+{
+  if(empty($conAuth))return;
+  $lKey='';
+  foreach($conAuth as $k=>&$v){
+    #$dbAuth与$conAuth的键名有一个转换关系类似$dbAuth['iss']['pat']与$conAuth['_ISS']['_PAT']
+    if(strpos($k,'_')===0){
+      $lKey=strtolower(ltrim($k,"_"));
+    }else{
+      $lKey=$k;
+    }
+    
+    if(is_array($v) && isset($dbAuth[$lKey])){
+      fn_merge_auth($dbAuth[$lKey],$v);
+    }else {
+      #因上述的转换关系就需要对$dbAuth与$conAuth进行逐层比对，复制。
+      #如果无上述的转换关系，2个数组键名一样就只要一条语句：$dbAuth[$lKey]=$v;
+      if(isset($dbAuth[$lKey]) && $dbAuth[$lKey]){
+        $dbAuth[$lKey]=1;
+      }else if(is_array($v)){
+        $dbAuth[$lKey]=[];
+        #递归调用自己
+		fn_merge_auth($dbAuth[$lKey],$v);
+      }else{
+        $dbAuth[$lKey]=$v;
+      }
+    }
+  }
+  return $dbAuth;
+}
+
+?>
